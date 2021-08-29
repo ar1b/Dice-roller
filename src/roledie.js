64ad782bf2e4915ec57155ffb9335f10189d1952
arriiibb <arib.kamal@ryerson.ca>
2021-08-28 22:47:06 -0400

    Built content and dice roller logic

---
+++ src/roledie.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import Die from './die';
+import './rolldice.css';
+
+class RollDie extends Component {
+    static defaultProps = {
+        sides: ["one", "two", "three", "four","five","six"]
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {die1: 'one', die2: 'one', rolling: false};
+        this.roll = this.roll.bind(this);
+    }
+
+    roll() {
+        const newDie1 = this.props.sides[
+            Math.floor(Math.random() * this.props.sides.length)
+        ];
+
+        const newDie2 = this.props.sides[
+            Math.floor(Math.random() * this.props.sides.length)
+        ];
+
+        this.setState({die1: newDie1, die2: newDie2, rolling: true});
+
+        setTimeout(() =>{
+            this.setState({rolling: false});
+        },500);
+    }
+
+    render() {
+        return (
+        <div className = "rolldice" >
+            <div className = "rolldice-container">
+                <Die face={this.state.die1}/>
+                <Die face={this.state.die2}/>
+            </div>
+            <button onClick={this.roll} disabled={this.state.rolling}>
+                {this.state.rolling ? "Rolling.." : "Roll Dice" }
+            </button>
+        </div>
+        )
+    }
+}
+
+export default RollDie;
